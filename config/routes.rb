09f4c4da5a605871f152b2282c382cecdc4adc1e
brandon <brandon@bethel-churchs-imac.local>
2011-03-08 16:50:45 -0800

    Authentication required

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Shorty::Application.routes.draw do
   
   match "/auth/:provider/callback" => "sessions#create"
   match "/auth/failure" => "sessions#failure"
+  match "/signout" => "sessions#destroy", :as => :signout
 
   resources :shorts do
     resources :visits
