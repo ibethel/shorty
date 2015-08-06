63bf82625689b0fd084fc3a322435cfe62b2bd4d
Andrew Lyric <talyric@gmail.com>
2015-08-06 09:58:12 -0700

    pvcify

--- config/deploy/alpha.rb
+++ config/deploy/alpha.rb
@@ -8,3 +8,17 @@ set :rvm_ruby_version, '2.2.2'
 set :ssh_options, {port: 4774}
 
 server '162.243.43.149', roles: %w{web app db}, user: 'deploy' # server: web_1
+
+namespace :deploy do
+  Rake::Task["deploy:migrate"].clear_actions # Don't run migrations...just do the reload.
+  desc 'Overrides db:migrate to just run db:reload'
+  task :migrate => [:set_rails_env] do
+    on primary fetch(:migration_role) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, "db:reload"
+        end
+      end
+    end
+  end
+end
