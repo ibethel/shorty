3cbc6786ddbca5f3555c8be04bd2ce6c6eeca176
Andrew Lyric <talyric@gmail.com>
2015-08-06 11:39:36 -0700

    pvcify

--- config/deploy/preview.rb
+++ config/deploy/preview.rb
@@ -1,3 +1,10 @@
-server "173.255.215.22", :app, :web, :db, primary: true
-set :deploy_to, "/sites/preview.bcrr.us"
-set :rails_env, "preview"
+# This is a generated file.  Do not modify...or else!  :)
+
+set :stage, :preview
+set :rails_env, :preview # workaround for RAILS_ENV= being blank in delayed job tasks
+set :deploy_to, '/sites/shorty/preview'
+set :linked_files, ['.env.preview']
+set :rvm_ruby_version, '2.2.2'
+set :ssh_options, {port: 4774}
+
+server '162.243.43.149', roles: %w{web app db}, user: 'deploy' # server: web_1
