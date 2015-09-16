5502deb977045847c9151d7dfd781d9b0958e352
Ben Jacobs <benjamincjacobs@gmail.com>
2015-09-16 11:30:45 -0700

    update migrate back

--- db/migrate/20150526213406_create_embedded_forms.rb
+++ db/migrate/20150526213406_create_embedded_forms.rb
@@ -1,12 +1,12 @@
 class CreateEmbeddedForms < ActiveRecord::Migration
   def change
-    # create_table :embedded_forms do |t|
-    #   t.string :name
-    #   t.string :url_slug
-    #   t.string :service
-    #   t.string :form_url
-    #
-    #   t.timestamps null: false
-    # end
+    create_table :embedded_forms do |t|
+      t.string :name
+      t.string :url_slug
+      t.string :service
+      t.string :form_url
+
+      t.timestamps null: false
+    end
   end
 end
