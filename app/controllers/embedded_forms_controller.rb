95f0bb5433fccaab5d6eab955d8ff46611b57afa
Ben Jacobs <benjamincjacobs@gmail.com>
2015-09-23 14:21:33 -0700

    applied style updates based on new design

--- app/controllers/embedded_forms_controller.rb
+++ app/controllers/embedded_forms_controller.rb
@@ -1,6 +1,7 @@
 class EmbeddedFormsController < ApplicationController
   def show
     @form = EmbeddedForm.find_by(id: params[:id])
+    @page_title = "Event Registration"
 
     render_404 if @form.blank?
   end
