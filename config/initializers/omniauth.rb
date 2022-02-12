9455e4c53489e25b7842626b5bd107947a305328
dendomenko <dendomenko@gmail.com>
2022-02-13 01:08:24 +0200

    Force ssl in app

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,3 +4,4 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, '454507292205-ve8b7322mkkcomb324jnq6hq72l2eg14.apps.googleusercontent.com', '-4YKw7E7dsP7Vlcbma-aUKBJ', { :hd => "ibethel.org" }
 
 end
+OmniAuth.config.full_host = Rails.env.production? ? 'https://bcrr.us' : 'http://localhost:3000'
