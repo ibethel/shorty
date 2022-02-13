bddcf0d57ad7a074c266dc5d01b5cc54c36fa19b
dendomenko <dendomenko@gmail.com>
2022-02-13 09:32:06 +0200

    Tune config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,4 +3,5 @@ require 'openid/store/filesystem'
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, '454507292205-ve8b7322mkkcomb324jnq6hq72l2eg14.apps.googleusercontent.com', '-4YKw7E7dsP7Vlcbma-aUKBJ'
 end
+OmniAuth.config.allowed_request_methods = %i[get]
 OmniAuth.config.full_host = Rails.env.production? ? 'https://bcrr.us' : 'http://localhost:3000'
